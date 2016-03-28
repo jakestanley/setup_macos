d066dfdd7aeba23a1575545f39156b691681b7d8
Jake Stanley <prudentia713@gmail.com>
2016-03-28 13:07:26 +0100

    added README and install script

---
+++ README.md
@@ -0,0 +1,24 @@
+# OS X
+This repository contains scripts for setting up my OS X environment using mainly `brew`
+
+## Software to be installed
+
+## Manual installation
+- Parallels Desktop 10 - manual installation required as I'm only licensed for version 10
+
+### Packages via Homebrew
+`install.sh` installs a bunch of stuff using Brew.
+// TODO
+## Further configuration
+
+### Synchronise BitTorrent Sync folders
+
+So, yeah. Should be pretty self explanatory
+
+### Set up symbolic links for BitTorrent Sync files and folders
+- iTunes
+- Logic
+- GarageBand
+- Resources
+- Apple Loops
+`ln -s /Users/jake/Documents/BitTorrent\ Sync/Music\ Production/User\ Loops /Users/jake/Library/Audio/Apple Loops/User Loops`
---
+++ install.sh
@@ -0,0 +1,37 @@
+# Install command line tools
+xcode-select --install
+
+# Install and configure Homebrew
+ruby -e “$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)”
+brew doctor
+brew install caskroom/cask/brew-cask
+
+# Install command line applications
+brew install wget
+brew install vim
+brew install mplayer
+
+# Install graphical applications
+brew cask install atom
+brew cask install filezilla
+brew cask install google-chrome
+brew cask install handbrake
+brew cask install sublime-text
+brew cask install omnifocus
+brew cask install bittorrent-sync
+brew cask install dropbox
+brew cask install 1password
+brew cask install firefox
+brew cask install sourcetree
+brew cask install steam
+brew cask install sunvox
+brew cask install alfred
+brew cask install vlc
+brew cask install spotify
+brew cask install nvalt
+brew cask install flux
+brew cask install blender
+brew cask install xslimmer
+brew cask install evernote
+brew cask install slack
+brew cask install growlnotify
